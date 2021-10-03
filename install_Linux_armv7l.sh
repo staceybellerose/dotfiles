c1af99821420cbe05aaf120193474e2b3a3b61f9
Stacey Adams <stacey.belle.rose@gmail.com>
2021-10-03 08:54:04 -0500

    Update installer with new command line options
    Provides the ability to run only parts of the installation process easily, without other parts being run automatically.
    
    Comments added to machine-specific install scripts to indicate their usage.

--- install_Linux_armv7l.sh
+++ install_Linux_armv7l.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC1091,SC2034
 
+# Use this script for any Raspberry Pi-based specific installations
+
 source ./bin/utils.sh
 
 packages="$1"
--- install_Linux_x86_64.sh
+++ install_Linux_x86_64.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC1091,SC2034
 
+# Use this script for any Intel/AMD-based specific installations
+
 source ./bin/utils.sh
 
 packages="$1"
